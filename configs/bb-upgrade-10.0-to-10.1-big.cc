95a434719c724fcd06e2415ed2bacdf47f74e129
Elena Stepanova <elenst@montyprogram.com>
2017-03-07 13:50:54 +0200

    Minor updates to 10.0=>10.1 big upgrade configuration

--- configs/bb-upgrade-10.0-to-10.1-big.cc
+++ configs/bb-upgrade-10.0-to-10.1-big.cc
@@ -37,9 +37,9 @@ $combinations = [
 # Page size combinations
 # (32K and 64K values are only applicable to 10.1+ and MySQL 5.7, so they are not present here)
     [
-        '--mysqld=--loose-innodb-page-size=16K',
-        '--mysqld=--loose-innodb-page-size=8K',
-        '--mysqld=--loose-innodb-page-size=4K'
+        '--mysqld=--innodb-page-size=16K',
+        '--mysqld=--innodb-page-size=8K',
+        '--mysqld=--innodb-page-size=4K',
     ],
 
 # Compression combinations are only applicable to 10.1+, so they are not present here
@@ -48,12 +48,11 @@ $combinations = [
 # - both old and new server are unencrypted and don't have the plugin;
 # - old server is unencrypted and does not have the plugin, the new server is encrypted and has the plugin;
 # Other encryption combinations are only applicable to 10.1+, so they are not present here
-
     [
         '',
         '
             --mysqld2=--file-key-management
-            --mysqld2=--file-key-management-filename=/data/encryption_keys.txt
+            --mysqld2=--file-key-management-filename=$BUILD_HOME/encryption_keys.txt
             --mysqld2=--plugin-load-add=file_key_management.so
             --mysqld2=--innodb-encrypt-tables
             --mysqld2=--innodb-encrypt-log
@@ -61,7 +60,7 @@ $combinations = [
             --mysqld2=--aria-encrypt-tables=1
             --mysqld2=--encrypt-tmp-disk-tables=1
             --mysqld2=--encrypt-binlog
-        '
+        ',
     ],
 
 ]
