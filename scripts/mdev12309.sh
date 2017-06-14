6750e4feb2b34ee81f52578e08ad0588ee6d5257
Elena Stepanova <elenst@montyprogram.com>
2017-06-14 19:04:03 +0300

    Added test 3 -- several tablespaces, different table flags

--- scripts/mdev12309.sh
+++ scripts/mdev12309.sh
@@ -20,7 +20,7 @@ if [ -z "$2" ] ; then
 fi
 
 ml="bin/mysql -uroot --protocol=tcp test"
-mupgrade="bin/mysql_upgrade -uroot --protocol=tcp --silent"
+mupgrade="bin/mysql_upgrade -uroot --protocol=tcp"
 
 case $testnum in
   "1")
@@ -41,6 +41,31 @@ case $testnum in
 	sql_old="$sql_old ; CREATE TABLE t1 (i int) TABLESPACE space1"
 	sql_new="SHOW CREATE TABLE t1"
   ;;
+  "3")
+	echo "-----------------------------------------------"
+	echo "Test $testnum: Migration of several tablespaces with several tables"
+	echo "-----------------------------------------------"
+	sql_old="CREATE TABLESPACE space1 ADD DATAFILE 'space1f1.ibd'"
+	sql_old="$sql_old ; CREATE TABLE t1 (i int) TABLESPACE space1"
+	sql_old="$sql_old ; INSERT INTO t1 VALUES (1),(2)"
+	sql_old="$sql_old ; CREATE TABLE t2 (i int) ROW_FORMAT=COMPACT TABLESPACE space1"
+	sql_old="$sql_old ; INSERT INTO t2 VALUES (1),(2)"
+	sql_old="$sql_old ; CREATE TABLE t3 (i int) PACK_KEYS=1 TABLESPACE space1"
+	sql_old="$sql_old ; INSERT INTO t3 VALUES (1),(2)"
+	sql_old="$sql_old ; CREATE TABLESPACE space2 ADD DATAFILE 'space2f1.ibd' FILE_BLOCK_SIZE=8192"
+	sql_old="$sql_old ; CREATE TABLE t4 (i int) ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8 TABLESPACE space2"
+	sql_old="$sql_old ; INSERT INTO t4 VALUES (1),(2)"
+	sql_old="$sql_old ; CREATE TABLE t5 (i int) TABLESPACE space2 KEY_BLOCK_SIZE=8"
+	sql_old="$sql_old ; INSERT INTO t5 VALUES (1),(2)"
+	sql_old="$sql_old ; CREATE TABLE tsimple (i int)"
+	sql_old="$sql_old ; INSERT INTO tsimple VALUES (1),(2)"
+	sql_new="SHOW CREATE TABLE t1"
+	sql_new="$sql_new ; SHOW CREATE TABLE t2"
+	sql_new="$sql_new ; SHOW CREATE TABLE t3"
+	sql_new="$sql_new ; SHOW CREATE TABLE t4"
+	sql_new="$sql_new ; SHOW CREATE TABLE t5"
+	sql_new="$sql_new ; SHOW CREATE TABLE tsimple"
+  ;;
   *)
 	echo "Unknown test $testnum"
 	exit 1
@@ -54,7 +79,7 @@ cd $old_server
 rm -rf data
 cp -r $old_server_cleandir ./data
 start_server > /dev/null
-$ml -e "$sql_old"
+$ml -e "$sql_old" --force
 $ml -e "set global innodb_fast_shutdown=0; shutdown"
 sleep 3
 
@@ -72,7 +97,7 @@ $ml -e shutdown
 sleep 3
 mv data/log.err data/log.err.old
 start_server > /dev/null
-$ml -e "$sql_new"
+$ml -e "$sql_new" --force
 $ml -e shutdown
 sleep 3
-grep -iE 'warning|error' data/log.err
+! grep -iE 'warning|error' data/log.err
