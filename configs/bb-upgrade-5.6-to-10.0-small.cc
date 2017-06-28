cde7ff2bc2d944f6acad387e71222dfe42718309
Elena Stepanova <elenst@montyprogram.com>
2017-06-28 23:34:20 +0300

    Remove encryption combinations as they are not applicable to 10.0

--- configs/bb-upgrade-5.6-to-10.0-small.cc
+++ configs/bb-upgrade-5.6-to-10.0-small.cc
@@ -39,26 +39,8 @@ $combinations = [
     ],
 
 # Compression combinations are only applicable to 10.1+, so they are not present here
+# Encryption combinations are only applicable to 10.1+, so they are not present here
 
-# Encryption combinations
-# - both old and new server are unencrypted and don't have the plugin;
-# - old server is unencrypted and does not have the plugin, the new server is encrypted and has the plugin;
-# Other encryption combinations are only applicable to 10.1+, so they are not present here
-
-    [
-        '',
-        '
-            --mysqld2=--file-key-management
-            --mysqld2=--file-key-management-filename=$BUILD_HOME/encryption_keys.txt
-            --mysqld2=--plugin-load-add=file_key_management.so
-            --mysqld2=--innodb-encrypt-tables
-            --mysqld2=--innodb-encrypt-log
-            --mysqld2=--innodb-encryption-threads=4
-            --mysqld2=--aria-encrypt-tables=1
-            --mysqld2=--encrypt-tmp-disk-tables=1
-            --mysqld2=--encrypt-binlog
-        '
-    ],
 # File formats
     [
         '--mysqld=--innodb-file-format=Antelope',
