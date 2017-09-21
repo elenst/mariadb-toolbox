0c7a01918da2827ce82a45d4675378930b56bcb8
Elena Stepanova <elenst@montyprogram.com>
2017-09-21 20:07:27 +0300

    Fix paths to init files

--- configs/buildbot-transform.cc
+++ configs/buildbot-transform.cc
@@ -1,7 +1,7 @@
 $combinations = [
     [
     '
-	--appverif
+        --appverif
         --no-mask
         --seed=time
         --threads=5
@@ -16,14 +16,14 @@ $combinations = [
     [
 		'--grammar=conf/mariadb/optimizer.yy',
 		'--grammar=conf/mariadb/optimizer.yy --notnull',
-		'--grammar=conf/mariadb/optimizer.yy --skip-gendata --mysqld=--init-file=$RQG_HOME/conf/mariadb/world.sql',
+		'--grammar=conf/mariadb/optimizer.yy --skip-gendata --mysqld=--init-file='.getcwd().'/conf/mariadb/world.sql',
 		'--grammar=conf/optimizer/range_access2.yy --gendata=conf/optimizer/range_access2.zz',
 		'--grammar=conf/optimizer/range_access.yy --gendata=conf/optimizer/range_access.zz',
 		'--grammar=conf/optimizer/outer_join.yy --gendata=conf/optimizer/outer_join.zz',
 		'--grammar=conf/optimizer/optimizer_access_exp.yy --gendata=conf/optimizer/range_access.zz',
 		'--grammar=conf/optimizer/optimizer_access_exp.yy --notnull',
 		'--grammar=conf/optimizer/optimizer_access_exp.yy',
-		'--grammar=conf/optimizer/optimizer_access_exp.yy --skip-gendata --mysqld=--init-file=$RQG_HOME/conf/mariadb/world.sql',
+		'--grammar=conf/optimizer/optimizer_access_exp.yy --skip-gendata --mysqld=--init-file='.getcwd().'/conf/mariadb/world.sql',
     ]
 ];
 
