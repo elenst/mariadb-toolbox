349e74d837f2d807fa73597d539463a056154569
Elena Stepanova <elenst@montyprogram.com>
2018-08-13 00:32:06 +0300

    Avoid using exit, it causes troubles

--- travis/run_single_test.sh
+++ travis/run_single_test.sh
@@ -18,14 +18,20 @@ CURTIME=`date '+%s'`
 
 test_result=0
 
+function soft_exit
+{
+  return $test_result
+}
+
 if [ $((JOB_END - CURTIME)) -gt $((TEST_DURATION + 60)) ] ; then
   cd $RQG_HOME
   perl ./runall-new.pl --basedir=$BASEDIR --vardir=$LOGDIR/vardir $GLOBAL_RQG_OPTIONS $JOB_RQG_OPTIONS $TEST_RQG_OPTIONS > $LOGDIR/trial.log 2>&1
   test_result=$?
   . $SCRIPT_DIR/collect_single_failure_info.sh
-  exit $test_result
 else
   echo "Too little time left, skipping the test"
 fi
+
+soft_exit
 
 set +x
