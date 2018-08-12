ac806d06b52420f309c0af3ffe568b01ff7f17ae
Elena Stepanova <elenst@montyprogram.com>
2018-08-13 02:11:28 +0300

    Improve output

--- travis/run_single_test.sh
+++ travis/run_single_test.sh
@@ -10,8 +10,6 @@
 # - $TEST_RQG_OPTIONS (optional)
 # - $TEST_DURATION (optional)
 
-set -x
-
 TEST_DURATION="${TEST_DURATION:-600}"
 
 CURTIME=`date '+%s'`
@@ -20,13 +18,13 @@ test_result=0
 
 if [ $((JOB_END - CURTIME)) -gt $((TEST_DURATION + 60)) ] ; then
   cd $RQG_HOME
+  date
   perl ./runall-new.pl --basedir=$BASEDIR --vardir=$LOGDIR/vardir $GLOBAL_RQG_OPTIONS $JOB_RQG_OPTIONS $TEST_RQG_OPTIONS > $LOGDIR/trial.log 2>&1
   test_result=$?
+  date
   . $SCRIPT_DIR/collect_single_failure_info.sh
 else
   echo "Too little time left, skipping the test"
 fi
 
 . $SCRIPT_DIR/soft_exit.sh $test_result
-
-set +x
