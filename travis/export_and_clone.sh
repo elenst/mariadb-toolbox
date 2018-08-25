d9b36ee489ff4431664f75122ea3e5dbcf72b101
Elena Stepanova <elenst@montyprogram.com>
2018-08-26 00:10:37 +0300

    Use RQG from MariaDB randgen repo

--- travis/export_and_clone.sh
+++ travis/export_and_clone.sh
@@ -16,6 +16,7 @@ export SCRIPT_DIR=$TOOLBOX_DIR/travis
 export SRCDIR=$HOME/src
 export BASEDIR=$HOME/server
 export RQG_HOME=$HOME/rqg
+export RQG_BRANCH="${RQG_BRANCH:-elenst-dev}"
 export RQG_BRANCH="${RQG_BRANCH_OVERRIDE:-$RQG_BRANCH}"
 export SERVER_BRANCH="${SERVER_BRANCH_OVERRIDE:-$SERVER_BRANCH}"
 export LOGDIR=$HOME/logs
@@ -24,7 +25,7 @@ if [ -n "$SERVER_BRANCH" ] ; then SERVER_BRANCH_OPTION="--branch $SERVER_BRANCH"
 if [ -n "$RQG_BRANCH" ] ; then RQG_BRANCH_OPTION="--branch $RQG_BRANCH" ; fi
 
 git clone https://github.com/MariaDB/server --depth=1 $SERVER_BRANCH_OPTION $SRCDIR
-git clone https://github.com/elenst/rqg --depth=1 $RQG_BRANCH_OPTION $RQG_HOME
+git clone https://github.com/MariaDB/randgen --depth=1 $RQG_BRANCH_OPTION $RQG_HOME
 cd $RQG_HOME
 export RQG_REVISION=`git log -1 --pretty="%H"`
 echo $RQG_REVISION
