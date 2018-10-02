c6d172ddc85c73c9f5f36fb6ece45c5ac44e2c08
Elena Stepanova <elenst@montyprogram.com>
2018-10-02 03:27:44 +0300

    Print the steps

--- travis/get_old_version.sh
+++ travis/get_old_version.sh
@@ -15,6 +15,8 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
+set -x
+
 OLD=$1
 
 cd $HOME
@@ -55,3 +57,5 @@ else
   echo "Old and new versions are the same"
   ln -s $BASEDIR $HOME/old
 fi
+
+set +x
