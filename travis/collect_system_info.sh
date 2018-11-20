41c588552d0df15f814297750ee93fb3f63d7671
Elena Stepanova <elenst@montyprogram.com>
2018-11-20 03:22:59 +0200

    Try to override coredump location #2

--- travis/collect_system_info.sh
+++ travis/collect_system_info.sh
@@ -7,6 +7,6 @@ df -k $HOME
 cat /proc/cpuinfo | grep -E 'processor|cpu cores|cpu MHz|model name'
 top -b -n 1
 
-echo "core" | sudo tee /proc/sys/kernel/core_pattern
+sudo sh -c "echo core > /proc/sys/kernel/core_pattern"
 
 set +x
