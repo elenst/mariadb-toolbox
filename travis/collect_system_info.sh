94fba2cbcc15090221357083d50bdc28ad2fccdd
Elena Stepanova <elenst@montyprogram.com>
2018-11-20 18:21:48 +0200

    More search for lost coredumps

--- travis/collect_system_info.sh
+++ travis/collect_system_info.sh
@@ -7,6 +7,9 @@ df -k $HOME
 cat /proc/cpuinfo | grep -E 'processor|cpu cores|cpu MHz|model name'
 top -b -n 1
 
+dpkg -l | grep apport
+sudo ps -ef | grep apport
+
 cat /proc/sys/kernel/core_pattern
 sudo service apport stop
 cat /proc/sys/kernel/core_pattern
