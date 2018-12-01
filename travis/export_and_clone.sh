6aa2002171e395db1adf9dca7213402a8cb4d881
Elena Stepanova <elenst@montyprogram.com>
2018-12-01 18:13:54 +0200

    Fix the path to the environment setup script

--- travis/export_and_clone.sh
+++ travis/export_and_clone.sh
@@ -3,11 +3,14 @@
 # This script is now really pointless, but too many Travis branches call it,
 # so it's easier to keep it than modify all travis configs
 
+# Expects
+# - TOOLBOX_DIR (mandatory)
+
 cd $HOME
 
 set -x
 
-. $SCRIPT_DIR/setup_environment.sh
+. $TOOLBOX_DIR/travis/setup_environment.sh
 . $SCRIPT_DIR/clone_server.sh
 
 set +x
