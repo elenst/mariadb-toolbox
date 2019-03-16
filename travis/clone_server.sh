c9987f4388c774f89b19321ac97bf89e6bb9ef5c
Elena Stepanova <elenst@montyprogram.com>
2019-03-16 19:40:01 +0200

    Allow configurable SERVER_REVISION

--- travis/clone_server.sh
+++ travis/clone_server.sh
@@ -4,13 +4,20 @@
 # - HOME (mandatory)
 # - SRCDIR (mandatory)
 # - SERVER_BRANCH_OPTION (optional)
+# - SERVER_REVISION (optional)
 
 cd $HOME
 
 set -x
 
-git clone https://github.com/MariaDB/server --depth=1 $SERVER_BRANCH_OPTION $SRCDIR
-cd $SRCDIR
+if [ -z "$SERVER_REVISION" ] ; then
+  git clone https://github.com/MariaDB/server --depth=1 $SERVER_BRANCH_OPTION $SRCDIR
+  cd $SRCDIR
+else
+  git clone https://github.com/MariaDB/server $SERVER_BRANCH_OPTION $SRCDIR
+  cd $SRCDIR
+  git checkout $SERVER_REVISION
+fi
 git log -1
 
 set +x
