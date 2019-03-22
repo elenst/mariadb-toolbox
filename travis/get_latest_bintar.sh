b28d266aeca241c19e5eec4e026163bab05b055b
Elena Stepanova <elenst@montyprogram.com>
2019-03-22 02:12:19 +0200

    Make the script more universal to allow configurable bintars

--- travis/get_latest_bintar.sh
+++ travis/get_latest_bintar.sh
@@ -4,14 +4,15 @@ if [ -z "$1" ] ; then
 fi
 
 SERVER_BRANCH=$1
+BINTAR_TYPE=${2-"kvm-bintar-trusty-amd64"}
 
 wget -nv -O index1.html http://hasky.askmonty.org/archive/$SERVER_BRANCH/
 builds=`grep 'a href="build-' index1.html | sed -e 's/.*href="\(build-[0-9]*\).*/\1/g' | sort -r | xargs`
 
 for b in $builds ; do
-  wget -nv -O index_$b.html http://hasky.askmonty.org/archive/$SERVER_BRANCH/$b/kvm-bintar-trusty-amd64/
+  wget -nv -O index_$b.html http://hasky.askmonty.org/archive/$SERVER_BRANCH/$b/$BINTAR_TYPE/
   if [ -e index_$b.html ] ; then
-    tarball=`grep 'a href="mariadb-' index_$b.html | sed -e 's/.*href="\(mariadb-.*64\)\.tar\.gz".*/\1/'`
+    tarball=`grep 'a href="mariadb-' index_$b.html | sed -e 's/.*href="\(mariadb-.*\)\.tar\.gz".*/\1/'`
     if [ -n "$tarball" ] ; then
       echo "Found $tarball"
       break
@@ -20,11 +21,13 @@ for b in $builds ; do
 done
 
 if [ -n "$tarball" ] ; then
-  wget -nv http://hasky.askmonty.org/archive/$SERVER_BRANCH/$b/kvm-bintar-quantal-amd64/$tarball.tar.gz
-  tar zxf $tarball.tar.gz
+  wget -nv http://hasky.askmonty.org/archive/$SERVER_BRANCH/$b/$BINTAR_TYPE/$tarball.tar.gz
   rm -rf server
-  mv $tarball server
+  mkdir server
+  cd server
+  tar zxf ../$tarball.tar.gz --strip-components=1
+  cd ..
 else
   echo "Could not find the server bintar"
-  $SCRIPT_DIR/soft_exit.sh 1
+  . $SCRIPT_DIR/soft_exit.sh 1
 fi
