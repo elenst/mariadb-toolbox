624e602ad4802105e7650e5cc0dcc5c03d0ac97b
Elena Stepanova <elenst@mariadb.com>
2019-08-25 02:04:01 +0300

    Use RQG simplification

--- scripts/reprobug.pl
+++ scripts/reprobug.pl
@@ -67,9 +67,10 @@ unless (defined $output) {
     exit 1;
 }
 
-$logdir= '/dev/shm' unless defined $logdir;
 $mtr_thread= 400 unless defined $mtr_thread;
 $rqg_home= $ENV{RQG_HOME} unless defined $rqg_home;
+$logdir= '/dev/shm' unless defined $logdir;
+my $workdir= $logdir.'/repro_'.$mtr_thread;
 
 $SIG{INT}  = sub { register_repro_stage('aborted'); exit(1) };
 $SIG{TERM} = sub { register_repro_stage('aborted'); exit(1) };
@@ -81,14 +82,14 @@ print "Initial options:\n";
 print "    Search pattern: $output\n";
 print "    Basedir: $basedir\n";
 print "    Config file: $cnf_file\n";
-print "    Logdir: $logdir\n";
+print "    Workdir: $workdir\n";
 print "    RQG home: $rqg_home\n";
 print "    Server log: $server_log\n";
 print "    Test ID: $test_id\n\n";
 
 my @mtr_options= (
     '--testcase-timeout=120',
-    '--vardir='.$logdir.'/repro_vardir_'.$mtr_thread.'_mtr',
+    '--vardir='.$workdir.'/mtr',
     '--mysqld=--innodb',
     '--mysqld=--default-storage-engine=InnoDB',
     '--mysqld=--partition',
@@ -112,8 +113,7 @@ if ($basedir =~ /10\.[4-9]/) {
     push @mtr_options, '--mysqld=--use_stat_tables=PREFERABLY';
 }
 
-my @rqg_mandatory_options= ();
-my @rqg_removable_options= ();
+my @rqg_options= ();
 my $rqg_threads;
 
 foreach my $o (@ARGV) {
@@ -122,15 +122,12 @@ foreach my $o (@ARGV) {
     
     if ($o =~ /^--mysqld1?=--server[-_]id=\d+/) {
         # MTR doesn't like custom server IDs, only keep it for RQG
-        push @rqg_removable_options, $o;
+        push @rqg_options, $o;
     }
     elsif ($o =~ /^(?:--mysqld1?|--ps-protocol)/) {
         $o =~ s/--mysqld1/--mysqld/;
         push @mtr_options, $o;
-        push @rqg_removable_options, $o;
-    }
-    elsif ($o =~ /^(?:--grammar|--gendata=|--duration|--skip-gendata)/) {
-        push @rqg_mandatory_options, $o;
+        push @rqg_options, $o;
     }
     elsif ($o =~ /^--threads=(\d+)/) {
         $rqg_threads= $1;
@@ -139,92 +136,80 @@ foreach my $o (@ARGV) {
         $rqg_home= $1 unless defined $rqg_home;
     }
     elsif ($o =~ /^--/) {
-        push @rqg_removable_options, $o;
+        push @rqg_options, $o;
     }
 }
 
 $rqg_threads= 10 unless defined $rqg_threads;
 
-push @rqg_mandatory_options, '--basedir='.$basedir;
-push @rqg_mandatory_options, '--mtr-build-thread='.$mtr_thread;
-push @rqg_mandatory_options, '--vardir='.$logdir.'/repro_vardir_'.$mtr_thread.'_rqg';
-push @rqg_mandatory_options, '--trials=10';
+push @rqg_options, '--basedir='.$basedir;
+push @rqg_options, '--mtr-build-thread='.$mtr_thread;
+push @rqg_options, '--trials=5';
 
 my $result= 1;
-my $stage= 'initial';
 
 if (defined $server_log and -e $server_log) {
-    register_repro_stage('MTR: ' . $stage);
+    register_repro_stage('MTR: original run');
     $result= mtr_simplification($server_log);
 } else {
-    print "General log not provided, running RQG first\n";
-    register_repro_stage('RQG: '. $stage);
-    my $res= rqg_trials();
+    print "General log not provided, running RQG trials first\n";
+    register_repro_stage('RQG: original run');
+    my $res= rqg_trials($workdir.'/rqg_trials');
     if ($res != 0) {
         print "RQG trials failed to reproduce, giving up\n";
-        finalize(1);
+        register_repro_stage("FAILED (on original runs)");
+        exit 1;
     }
-    $result= mtr_simplification($logdir.'/repro_vardir_'.$mtr_thread.'_rqg/mysql.log');
+    register_repro_stage('MTR: after RQG trials');
+    $result= mtr_simplification($workdir.'/rqg_trials/mysql.log');
+    if ($result == 0) {
+        register_repro_stage("SUCCEEDED (on MTR simplification)");
+        exit 0;
+    }
 }
 
-while ($result != 0)
-{
-    if (scalar @rqg_removable_options) {
-        $stage= '' . scalar(@rqg_removable_options) . ' RQG option(s) left';
-        my $opt= shift @rqg_removable_options;
-        print "Trying to remove option $opt from the command line\n";
-        register_repro_stage('RQG: ' . $stage);
-        my $res= rqg_trials();
-        if ($res != 0) {
-            print "RQG trials failed to reproduce, keeping the options\n";
-            push @rqg_mandatory_options, $opt;
-            next;
-        }
-    } elsif ($rqg_threads > 1) {
-        $rqg_threads--;
-        $stage= "$rqg_threads RQG thread(s) left";
-        print "Trying to reduce the number of threads to $rqg_threads\n";
-        register_repro_stage('RQG: ' . $stage);
-        my $res= rqg_trials();
-        if ($res != 0) {
-            print "RQG trials failed to reproduce, giving up\n";
-            finalize(1);
-        }
-    } else {
-        print "Ran out of options to try, giving up\n";
-        finalize(1);
-    }
-    unless (-e $logdir.'/repro_vardir_'.$mtr_thread.'_rqg/mysql.log') {
-        print "RQG run didn't produce general log";
-        next;
-    }
-    if (-e $logdir.'/repro_vardir_'.$mtr_thread.'_rqg/my.cnf') {
-        $cnf_file= $logdir.'/repro_vardir_'.$mtr_thread.'_rqg/my.cnf';
-    }
-    register_repro_stage('MTR: ' . $stage);
-    $result= mtr_simplification($logdir.'/repro_vardir_'.$mtr_thread.'_rqg/mysql.log');
+print "MTR simplification of the original log failed, trying RQG simplification\n";
+register_repro_stage("RQG: simplification");
+my $rqg_result= rqg_simplification($workdir.'/rqg_simplification');
+if ($rqg_result != 0) {
+    print "RQG simplification failed, giving up\n";
+    register_repro_stage("FAILED (on RQG simplification)");
+    exit 1;
 }
 
-finalize(0);
-
-
-sub finalize {
-    my $res= shift;
-    $res ? register_repro_stage('failed') : register_repro_stage('succeeded');
-    exit $res;
+$result= mtr_simplification($workdir.'/rqg_simplification/vardir/mysql.log');
+if ($result == 0) {
+    register_repro_stage("SUCCEEDED (on MTR simplification)");
+} else {
+    print "MTR simplifcication failed, staying with simplified RQG grammar\n";
+    register_repro_stage("SUCCEEDED (on RQG simplification)");
 }
-
+exit 0;
 
 sub rqg_trials {
+    my $vardir= shift;
     unless (defined $rqg_home) {
         print "ERROR: RQG home is not defined, cannot run the test\n";
-        finalize(1);
+        register_repro_stage("Failed: RQG_HOME not defined");
+        exit 1;
     }
-    print "Running RQG test: perl $rqg_home/runall-trials.pl @rqg_mandatory_options @rqg_removable_options --threads=$rqg_threads > $logdir/repro_trials_$mtr_thread.log\n";
-    system("cd $rqg_home; perl $rqg_home/runall-trials.pl --output=\"$output\" @rqg_mandatory_options @rqg_removable_options --threads=$rqg_threads > $logdir/repro_trials_$mtr_thread.log 2>&1");
+    print "Running RQG test: perl $rqg_home/runall-trials.pl @rqg_options --threads=$rqg_threads > $workdir/rqg_trials.out\n";
+    system("cd $rqg_home; perl $rqg_home/runall-trials.pl --vardir=$vardir --output=\"$output\" @rqg_options --threads=$rqg_threads > $workdir/rqg_trials.out 2>&1");
     my $res= $?>>8;
-    system("grep -E 'will exit with exit status|exited with exit status' $logdir/repro_trials_$mtr_thread.log");
+    system("grep -E 'will exit with exit status|exited with exit status' $workdir/rqg_trials.out");
     return $res;
+}
+
+sub rqg_simplification {
+    my $wdir= shift;
+    unless (defined $rqg_home) {
+        print "ERROR: RQG home is not defined, cannot run the simplification\n";
+        register_repro_stage("Failed: RQG_HOME not defined");
+        exit 1;
+    }
+    print "Running RQG simplification: perl $rqg_home/util/simplify-rqg-test.pl --workdir=$wdir --output=\"$output\" @rqg_options --threads=$rqg_threads > $workdir/rqg_simplification.out\n";
+    system("cd $rqg_home; perl $rqg_home/util/simplify-rqg-test.pl --workdir=$wdir --output=\"$output\" @rqg_options --threads=$rqg_threads > $workdir/rqg_simplification.out 2>&1");
+    return $?>>8;
 }
 
 sub mtr_simplification {
@@ -242,12 +227,12 @@ sub mtr_simplification {
     print "Log file size: ".(-s $log)." ($log)\n";
     print "Test file size: ".(-s "$suitedir/${testname}.test")." ($suitedir/${testname}.test)\n\n";
     print "Running simplification\n";
-    my $cmd1= "cd $basedir/mysql-test; perl $scriptdir/simplify-mtr-test.pl --initial-trials=10 --suitedir=$suitedir --testcase=$testname --options=\"$cnf_options @mtr_options @mtr_timeouts\" --output=\"$output\"";
-    my $cmd2= "cd $basedir/mysql-test; perl $scriptdir/simplify-mtr-test.pl --initial-trials=3 --suitedir=$suitedir --testcase=$testname --options=\"$cnf_options @mtr_options\" --output=\"$output\"";
+    my $cmd1= "cd $basedir/mysql-test; perl $scriptdir/simplify-mtr-test.pl --initial-trials=1 --suitedir=$suitedir --testcase=$testname --options=\"$cnf_options @mtr_options @mtr_timeouts\" --output=\"$output\"";
+#    my $cmd2= "cd $basedir/mysql-test; perl $scriptdir/simplify-mtr-test.pl --initial-trials=3 --suitedir=$suitedir --testcase=$testname --options=\"$cnf_options @mtr_options\" --output=\"$output\"";
     print "First attempt with low timeouts, if it doesn't work, try without them\n\n";
 
     my $res;
-    foreach my $cmd ($cmd1, $cmd2) {
+    foreach my $cmd ($cmd1) {
         print "Command line\n$cmd1\n\n";
         system($cmd);
         $res= $?>>8;
