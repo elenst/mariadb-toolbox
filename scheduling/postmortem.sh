11685683dfbf5cd2cccfb92d92f73f2f1c411623
Elena Stepanova <elenst@mariadb.com>
2019-08-31 21:34:31 +0300

    Switch to using collect_rqg_result_info.sh

--- scheduling/postmortem.sh
+++ scheduling/postmortem.sh
@@ -21,54 +21,34 @@ fi
 
 echo SERVER_BRANCH=$SERVER_BRANCH TEST_ALIAS=$TEST_ALIAS TEST_RESULT=$TEST_RESULT TEST_ID=$TEST_ID LOCAL_CI=$LOCAL_CI
 echo "Test result: $TEST_RESULT"
-grep -A 1 'Final command line' $LOGDIR/${PREFIX}trial.log
+
+vardirs=''
+for v in $LOGDIR/${PREFIX}vardir* ; do
+    vardirs="$vardir --vardir=$v"
+done
+
+`dirname $0`/../scripts/collect_rqg_result_info.sh $vardirs --test-log=$LOGDIR/${PREFIX}trial.log > $LOGDIR/${PREFIX}result_info 2>&1
+
+SERVER_BRANCH=$SERVER_BRANCH TEST_ALIAS=$TEST_ALIAS TEST_RESULT=$TEST_RESULT TEST_ID=$TEST_ID LOCAL_CI=$LOCAL_CI perl $RQG_HOME/util/check_for_known_bugs.pl $SIGNATURES $LOGDIR/${PREFIX}result_info > $LOGDIR/${PREFIX}matches 2>&1
+
+cat $LOGDIR/${PREFIX}matches
+cat $LOGDIR/${PREFIX}result_info
 
 for c in `find $LOGDIR/${PREFIX}vardir* -name core*` ; do
     binary=`file $c | sed -e "s/.*execfn: '\(.*\)', platform.*/\\1/"`
     core_pid=`echo $c | sed -e 's/.*core\.\([0-9]*\)$/\1/g'`
     gdb --batch --eval-command="thread apply all bt full" $binary $c > $LOGDIR/${PREFIX}threads.$core_pid 2>&1
-    gdb --batch --eval-command="bt" $binary $c > $LOGDIR/${PREFIX}stack.$core_pid 2>&1
-    echo "---------------------------------------------------------"
-    echo "--- Coredump $c"
-    gdb --batch --eval-command="bt" $binary $c | grep -v 'New LWP'
-    echo "---------------------------------------------------------"
 done
 
-SERVER_BRANCH=$SERVER_BRANCH TEST_ALIAS=$TEST_ALIAS TEST_RESULT=$TEST_RESULT TEST_ID=$TEST_ID LOCAL_CI=$LOCAL_CI perl $RQG_HOME/util/check_for_known_bugs.pl $SIGNATURES `find $LOGDIR/${PREFIX}vardir* -name mysql*.err*` `find $LOGDIR/${PREFIX}vardir* -name mbackup*.log` $LOGDIR/${PREFIX}stack.* --last=$LOGDIR/${PREFIX}trial.log > $LOGDIR/${PREFIX}matches 2>&1
-cat $LOGDIR/${PREFIX}matches
-
 cd $LOGDIR
-if [ $TEST_RESULT != "OK" ] ; then
-    grep -i -A 200 -E 'assertion|signal|\[FATAL\]|pure virtual method called' $LOGDIR/${PREFIX}vardir*/mysql.err $LOGDIR/${PREFIX}vardir*/mbackup_*
-    if [[ $TEST_RESULT =~ BACKUP_FAILURE|UPGRADE_FAILURE|RECOVERY_FAILURE|DEADLOCKED|TEST_FAILURE ]] ; then
-        echo "--- Errors in trial.log and mysql.err logs --------------"
-        grep ERROR $LOGDIR/${PREFIX}trial.log $LOGDIR/${PREFIX}vardir*/mysql.err*
-    fi
-    if [[ $TEST_RESULT =~ BACKUP_FAILURE|TEST_FAILURE ]] ; then
-        grep -i error $LOGDIR/${PREFIX}vardir*/mbackup_*
-    elif [[ $TEST_RESULT =~ DATABASE_CORRUPTION ]] ; then
-        echo "--- CORRUPTION in trial.log -----------------------------"
-        grep DATABASE_CORRUPTION $LOGDIR/${PREFIX}trial.log
-        echo "---------------------------------------------------------"
-    elif [[ $TEST_RESULT =~ CRITICAL_FAILURE|ALARM|ENVIRONMENT_FAILURE|UNKNOWN_ERROR|TEST_FAILURE|N\/A ]] ; then
-        echo "--- Beginning and end of trial.log ----------------------"
-        head -n 5 $LOGDIR/${PREFIX}trial.log
-        tail -n 100 $LOGDIR/${PREFIX}trial.log
-        echo "---------------------------------------------------------"
-    fi
-    for cnf in $LOGDIR/${PREFIX}vardir*/my.cnf ; do
-        echo "---" Config file in `dirname $cnf`
-        cat $cnf
-        echo "---------------------------------------------------------"
-    done
+if [ "$TEST_RESULT" != "OK" ] ; then
     if ! grep 'STRONG matches' $LOGDIR/${PREFIX}matches || grep -E 'FOUND FIXED|\[Draft\]' $LOGDIR/${PREFIX}matches ; then
         tar zcf archive/${PREFIX}vardir.tar.gz ${PREFIX}*
         echo "Archived all data: " `ls -l archive/${PREFIX}vardir.tar.gz`
-        rm $LOGDIR/${PREFIX}matches
     else
         echo "Strong matches found, data discarded"
     fi
     tar zcf archive/${PREFIX}repro.tar.gz `find ${PREFIX}vardir* -name mysql.log` `find ${PREFIX}vardir* -name my.cnf` ${PREFIX}postmortem
 fi
-rm -rf ${PREFIX}vardir*
-mv $LOGDIR/${PREFIX}* $LOGDIR/archive/
+rm -rf ${PREFIX}vardir* $LOGDIR/${PREFIX}matches $LOGDIR/${PREFIX}result_info
+mv $LOGDIR/${PREFIX}* $LOGDIR/archive/ > /dev/null 2>&1
