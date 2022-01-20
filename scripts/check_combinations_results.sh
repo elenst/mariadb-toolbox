c818242f67491963c25f3a7cf6b18722b67fc17e
Elena Stepanova <elenst@mariadb.com>
2022-01-21 01:19:02 +0200

    Treat files as text while grepping

--- scripts/check_combinations_results.sh
+++ scripts/check_combinations_results.sh
@@ -3,11 +3,13 @@
 vardirs=`ls | grep vardir1 | xargs`
 rqg=${1-"/data/src/rqg"}
 
+echo "RQG in use: $rqg"
+
 for v in $vardirs ; do
   log=`echo $v | sed -e 's/vardir1_\([0-9]*\)/trial\1.log/'`
   echo "###################################"
   echo "Log: $log"
-  grep 'runall-new.pl will exit with exit status' $log | sed -e 's/^.*runall-new.pl will exit with exit status/Result:/'
+  grep -a 'runall-new.pl will exit with exit status' $log | sed -e 's/^.*runall-new.pl will exit with exit status/Result:/'
 #  set -x
   perl $rqg/util/check_for_known_bugs.pl --signatures=$rqg/data/bug_signatures $v/mysql.err $log
 #  set +x
