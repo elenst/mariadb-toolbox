df11846843382af1bb51d458c8aaa864f70c7272
Elena Stepanova <elenst@mariadb.com>
2022-07-14 15:26:24 +0300

    More output, avoid division by zero

--- scripts/coverage_summary_per_source_file.pl
+++ scripts/coverage_summary_per_source_file.pl
@@ -14,10 +14,12 @@ use strict;
 # - total number of hits
 
 my %stats_per_file= ();
+my %stats_per_component= ();
 my $sourcedir; # To be defined based on lcov.info contents
 my $max_name_length= 0;
 my $fname;
 my $skip_patterns= qr/\/unittest|mysql-test\//;
+my $warnings='';
 
 my ($instrumented, $hit, $total_hits)= (0,0,0);
 while (<>) {
@@ -59,12 +61,46 @@ foreach my $f (sort keys %stats_per_file) {
   $fname= $f;
   $fname=~ s/$sourcedir//;
   ($instrumented, $hit, $total_hits) = @{$stats_per_file{$f}};
-  $total_instrumented+= $instrumented;
-  $total_hit+= $hit;
-  $grand_total_hits+= $total_hits;
-  print sprintf("| %${max_name_length}s | %10s | %10s | %7s | %16s |\n","$fname ",$instrumented,$hit,sprintf("%.2f%",100*$hit/$instrumented),$total_hits);
+  # Don't count external libraries in summary and don't list them per-file.
+  # They will be included into 'External' component though
+  if ($fname !~ /^\//) {
+    $total_instrumented+= $instrumented;
+    $total_hit+= $hit;
+    $grand_total_hits+= $total_hits;
+    print sprintf("| %${max_name_length}s | %10s | %10s | %7s | %16s |\n","$fname ",$instrumented,$hit,sprintf("%.2f%",100*($instrumented ? $hit/$instrumented : 0)),$total_hits);
+  }
+  my $component= 'Other';
+  if ($fname =~ /^\//) {
+    $component= 'External';
+  } elsif ($fname =~ /^(client|extra\/\w+|include|libmariadb|libmysqld|mysys|mysys_ssl|dbug|plugin\/\w+|sql-common|sql|storage\/\w+|strings|tests|tpool|vio|wsrep-lib)\//) {
+    $component= $1;
+  } else {
+    $warnings .= "WARNING: Could not recognize component for $fname\n";
+  }
+  if (not defined $stats_per_component{$component}) {
+    @{$stats_per_component{$component}}= (0,0,0);
+  }
+  my @stats= @{$stats_per_component{$component}};
+  $stats[0] += $instrumented;
+  $stats[1] += $hit;
+  $stats[2] += $total_hits;
+  @{$stats_per_component{$component}}= @stats;
 }
+print $separator;
+
+foreach my $c (sort keys %stats_per_component) {
+  next if $c eq 'External';
+  print sprintf("| %${max_name_length}s | %10s | %10s | %7s | %16s |\n","$c ",${$stats_per_component{$c}}[0],${$stats_per_component{$c}}[1],sprintf("%.2f%",100*(${$stats_per_component{$c}}[0] ? ${$stats_per_component{$c}}[1]/${$stats_per_component{$c}}[0] : 0)),${$stats_per_component{$c}}[2]);
+}
+
 print $separator;
 print sprintf("| %${max_name_length}s | %10s | %10s | %7s | %16s |\n","Total ",$total_instrumented,$total_hit,sprintf("%.2f%",100*$total_hit/$total_instrumented),$grand_total_hits);
 print $separator;
+if ($stats_per_component{'External'}) {
+  print sprintf("| %${max_name_length}s | %10s | %10s | %7s | %16s |\n","External ",${$stats_per_component{'External'}}[0],${$stats_per_component{'External'}}[1],sprintf("%.2f%",100*(${$stats_per_component{'External'}}[0] ? ${$stats_per_component{'External'}}[1]/${$stats_per_component{'External'}}[0] : 0)),${$stats_per_component{'External'}}[2]);
+  print $separator;
+}
 print "\n";
+if ($warnings) {
+  print $warnings;
+}
