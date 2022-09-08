58b83c1a3444d95d0737af286b0b8b21ce18dcae
Elena Stepanova <elenst@mariadb.com>
2022-09-09 01:01:31 +0300

    Make the script return exit code depending on the result

--- scripts/check_combinations_results.sh
+++ scripts/check_combinations_results.sh
@@ -5,13 +5,15 @@ rqg=${1-"/data/src/rqg"}
 
 echo "RQG in use: $rqg"
 
+res=0
 for v in $vardirs ; do
   log=`echo $v | sed -e 's/vardir1_\([0-9]*\)/trial\1.log/'`
   echo "###################################"
   echo "Log: $log"
   grep -a 'runall-new.pl will exit with exit status' $log | sed -e 's/^.*runall-new.pl will exit with exit status/Result:/'
-#  set -x
-  perl $rqg/util/check_for_known_bugs.pl --signatures=$rqg/data/bug_signatures* $v/mysql.err $log $v/boot.log
-#  set +x
+  if ! perl $rqg/util/check_for_known_bugs.pl --signatures=$rqg/data/bug_signatures* $v/mysql.err $log $v/boot.log ; then
+    res=1
+  fi
   echo "###################################"
 done
+(exit $res)
