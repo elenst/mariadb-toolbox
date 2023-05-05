bf131c7a0d1fc0d7a11f26202831b4dace4f2d4b
Elena Stepanova <elenst@mariadb.com>
2023-05-06 02:05:42 +0300

    Account for old and new signature location

--- scripts/check_combinations_results.sh
+++ scripts/check_combinations_results.sh
@@ -6,12 +6,20 @@ rqg=${1-"/data/src/rqg"}
 echo "RQG in use: $rqg"
 
 res=0
+if ls $rqg/util/bug_signatures* ; then
+  signatures=$rqg/util
+elif ls $rqg/data/bug_signatures* ; then
+  signatures=$rqg/data
+else
+  echo "ERROR: Signatures not found"
+  exit 1
+fi
 for v in $vardirs ; do
   log=`echo $v | sed -e 's/vardir1_\([0-9]*\)/trial\1.log/'`
   echo "###################################"
   echo "Log: $log"
-  grep -a 'runall-new.pl will exit with exit status' $log | sed -e 's/^.*runall-new.pl will exit with exit status/Result:/'
-  if ! perl $rqg/util/check_for_known_bugs.pl --signatures=$rqg/data/bug_signatures* $v/mysql.err $log $v/boot.log ; then
+#  grep -a 'Test run ends with exit status' $log | sed -e 's/^.*Test run ends with exit status/Result:/'
+  if ! perl $rqg/util/check_for_known_bugs.pl --signatures=$signatures/bug_signatures* $v/s*/mysql.err $log $v/s*/boot.log ; then
     res=1
   fi
   echo "###################################"
